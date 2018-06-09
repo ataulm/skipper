6d298d9de3872cb4814aef5ce18780536ecaaf42
Ataul Munim <hello@ataulm.com>
2018-06-09 11:06:25 +0100

    Remove unused import

--- service/src/main/java/com/ataulm/skipper/settings/SettingsActivity.kt
+++ service/src/main/java/com/ataulm/skipper/settings/SettingsActivity.kt
@@ -1,6 +1,5 @@
 package com.ataulm.skipper.settings
 
-import android.arch.lifecycle.Observer
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
